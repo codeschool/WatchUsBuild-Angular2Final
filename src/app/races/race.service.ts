ea3ec37e30aa658c10c0e4f52997d75e5497ab92
Sergio Cruz <hello@sergiocruz.me>
2017-04-12 15:53:18 -0400

    Upgraded to @angular/cli 1.0

--- src/app/races/race.service.ts
+++ src/app/races/race.service.ts
@@ -10,7 +10,7 @@ export class RaceService {
   constructor(private http: Http) { }
 
   getRaces() {
-    return this.http.get('app/races.json')
+    return this.http.get('assets/races.json')
           .map(response => <Race[]>response.json().racesData);
   }
 }
